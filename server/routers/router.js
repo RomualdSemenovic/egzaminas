3f4e3c59a79c292085282e82556cc5ea6c477c88
romuald <romuald.semenovic@gmail.com>
2021-05-20 11:28:01 +0300

    commit

--- server/routers/router.js
+++ server/routers/router.js
@@ -6,7 +6,7 @@ const middle = require('../middleware/middle')
 const controller = require('../controllers/main')
 
 router.post('/upload', middle.validator, controller.upload)
-router.get('/storage', controller.storage)
+router.get('/list', controller.list)
 router.get('/add/:id', controller.add)
 router.get('/remove/:id', middle.checkQuantity, controller.remove)
 router.get('/delete/:id', controller.delete)
