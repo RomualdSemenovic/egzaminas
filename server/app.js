618c2a063ce32771b0043c96a55fa03231c83260
romuald <romuald.semenovic@gmail.com>
2021-05-20 10:05:26 +0300

    commit

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
-const mainRouter = require('./routers/router')
+// const mainRouter = require('./routers/router')
 
 require('dotenv').config()
 
